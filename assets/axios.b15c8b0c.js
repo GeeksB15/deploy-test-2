04ac451e460eccb72afdd537ff7f14bbf4fa0a66
luiznogueira <luizetn@gmail.com>
2024-10-17 08:44:37 -0300

    .

---
+++ assets/axios.b15c8b0c.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.92328832.js";import{a as e}from"./index.92328832.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- assets/router.785f709c.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.34fa2a57.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
